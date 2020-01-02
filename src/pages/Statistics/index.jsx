2ed4080d3a5dff792849f1bf61088165ab2f0fa0
White Ace <whiteace@sina.com>
2020-01-02 16:36:26 +0800

    no message

---
+++ src/pages/Statistics/index.jsx
@@ -0,0 +1,113 @@
+import React, { Component, Suspense } from 'react';
+import { GridContent } from '@ant-design/pro-layout';
+import { connect } from 'dva';
+// import { Dropdown, Icon, Menu } from 'antd';
+import PageLoading from './components/PageLoading';
+// const TopSearch = React.lazy(() => import('./components/TopSearch'));
+// const ProportionSales = React.lazy(() => import('./components/ProportionSales'));
+
+import { getTimeDistance } from './utils/utils';
+import styles from './style.less';
+
+const IntroduceRow = React.lazy(() => import('./components/IntroduceRow'));
+const SalesCard = React.lazy(() => import('./components/SalesCard'));
+const TaskProgressTable = React.lazy(() => import('./components/TaskProgressTable'));
+
+class DashboardAnalysis extends Component {
+  state = {
+    rangePickerValue: getTimeDistance('year'),
+  };
+
+  reqRef = 0;
+
+  timeoutId = 0;
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    this.reqRef = requestAnimationFrame(() => {
+      dispatch({
+        type: 'dashboardAnalysis/fetch',
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'dashboardAnalysis/clear',
+    });
+    cancelAnimationFrame(this.reqRef);
+    clearTimeout(this.timeoutId);
+  }
+
+  handleRangePickerChange = rangePickerValue => {
+    const { dispatch } = this.props;
+    this.setState({
+      rangePickerValue,
+    });
+    dispatch({
+      type: 'dashboardAnalysis/fetchSalesData',
+    });
+  };
+
+  selectDate = type => {
+    const { dispatch } = this.props;
+    this.setState({
+      rangePickerValue: getTimeDistance(type),
+    });
+    dispatch({
+      type: 'dashboardAnalysis/fetchSalesData',
+    });
+  };
+
+  isActive = type => {
+    const { rangePickerValue } = this.state;
+    const value = getTimeDistance(type);
+
+    if (!rangePickerValue[0] || !rangePickerValue[1]) {
+      return '';
+    }
+
+    if (
+      rangePickerValue[0].isSame(value[0], 'day') &&
+      rangePickerValue[1].isSame(value[1], 'day')
+    ) {
+      return styles.currentDate;
+    }
+
+    return '';
+  };
+
+  render() {
+    const { rangePickerValue } = this.state;
+    const { dashboardAnalysis, loading } = this.props;
+    const { visitData, salesData } = dashboardAnalysis;
+
+    return (
+      <GridContent>
+        <React.Fragment>
+          <Suspense fallback={<PageLoading />}>
+            <IntroduceRow loading={loading} visitData={visitData} />
+          </Suspense>
+          <Suspense fallback={null}>
+            <SalesCard
+              rangePickerValue={rangePickerValue}
+              salesData={salesData}
+              isActive={this.isActive}
+              handleRangePickerChange={this.handleRangePickerChange}
+              selectDate={this.selectDate}
+            />
+          </Suspense>
+          <Suspense fallback={null}>
+            <TaskProgressTable loading={loading} />
+          </Suspense>
+        </React.Fragment>
+      </GridContent>
+    );
+  }
+}
+
+export default connect(({ dashboardAnalysis, loading }) => ({
+  dashboardAnalysis,
+  loading: loading.effects['dashboardAnalysis/fetch'],
+}))(DashboardAnalysis);
