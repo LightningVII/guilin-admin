ae111851cba511552ec90cebcfa8fb0462757fea
White Ace <whiteace@sina.com>
2020-01-02 17:40:52 +0800

    no message

--- src/pages/remote-sensing/list/index.jsx
+++ src/pages/remote-sensing/list/index.jsx
@@ -7,24 +7,62 @@ import {
   Divider,
   Dropdown,
   Form,
+  Input,
   Icon,
   Menu,
-  message,
+  // message,
+  Radio,
+  Modal,
   DatePicker,
+  Row,
+  Col,
 } from 'antd';
+
 import React, { useState, useEffect } from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import ProTable from '@ant-design/pro-table';
+// import ProTable from '@ant-design/pro-table';
 import router from 'umi/router';
 import { FormattedMessage } from 'umi-plugin-react/locale';
-import CreateForm from './components/CreateForm';
-import UpdateForm from './components/UpdateForm';
+// import CreateForm from './components/CreateForm';
+// import UpdateForm from './components/UpdateForm';
 import FeedbackList from './components/FeedbackList';
-import { queryRule, updateRule, addRule, removeRule } from './service';
+import { queryRule } from './service';
+// import { queryRule, updateRule, addRule, removeRule } from './service';
 import styles from './style.less';
 
 const { RangePicker } = DatePicker;
 const { TabPane } = Tabs;
+const { Group } = Radio;
+const radioStyle = {
+  display: 'block',
+  height: '30px',
+  lineHeight: '30px',
+};
+
+const menu = (
+  <Menu>
+    <Menu.Item key="1">
+      <Icon type="printer" />
+      打印
+    </Menu.Item>
+    <Menu.Item key="2">
+      <Icon type="copy" />
+      复制
+    </Menu.Item>
+    <Menu.Item key="3">
+      <Icon type="file-excel" />
+      Excel格式
+    </Menu.Item>
+    <Menu.Item key="4">
+      <Icon type="file" />
+      CSV格式
+    </Menu.Item>
+    <Menu.Item key="5">
+      <Icon type="file-pdf" />
+      PDF格式
+    </Menu.Item>
+  </Menu>
+);
 
 const operations = (
   <div className={styles.salesExtraWrap}>
@@ -73,77 +111,79 @@ const valueEnum = {
  * 添加节点
  * @param fields
  */
-const handleAdd = async fields => {
-  const hide = message.loading('正在添加');
+// const handleAdd = async fields => {
+//   const hide = message.loading('正在添加');
 
-  try {
-    await addRule({
-      desc: fields.desc,
-    });
-    hide();
-    message.success('添加成功');
-    return true;
-  } catch (error) {
-    hide();
-    message.error('添加失败请重试！');
-    return false;
-  }
-};
+//   try {
+//     await addRule({
+//       desc: fields.desc,
+//     });
+//     hide();
+//     message.success('添加成功');
+//     return true;
+//   } catch (error) {
+//     hide();
+//     message.error('添加失败请重试！');
+//     return false;
+//   }
+// };
 /**
  * 更新节点
  * @param fields
  */
 
-const handleUpdate = async fields => {
-  const hide = message.loading('正在配置');
+// const handleUpdate = async fields => {
+//   const hide = message.loading('正在配置');
 
-  try {
-    await updateRule({
-      name: fields.name,
-      desc: fields.desc,
-      key: fields.key,
-    });
-    hide();
-    message.success('配置成功');
-    return true;
-  } catch (error) {
-    hide();
-    message.error('配置失败请重试！');
-    return false;
-  }
-};
+//   try {
+//     await updateRule({
+//       name: fields.name,
+//       desc: fields.desc,
+//       key: fields.key,
+//     });
+//     hide();
+//     message.success('配置成功');
+//     return true;
+//   } catch (error) {
+//     hide();
+//     message.error('配置失败请重试！');
+//     return false;
+//   }
+// };
 /**
  *  删除节点
  * @param selectedRows
  */
 
-const handleRemove = async selectedRows => {
-  const hide = message.loading('正在删除');
-  if (!selectedRows) return true;
+// const handleRemove = async selectedRows => {
+//   const hide = message.loading('正在删除');
+//   if (!selectedRows) return true;
 
-  try {
-    await removeRule({
-      key: selectedRows.map(row => row.key),
-    });
-    hide();
-    message.success('删除成功，即将刷新');
-    return true;
-  } catch (error) {
-    hide();
-    message.error('删除失败，请重试');
-    return false;
-  }
-};
+//   try {
+//     await removeRule({
+//       key: selectedRows.map(row => row.key),
+//     });
+//     hide();
+//     message.success('删除成功，即将刷新');
+//     return true;
+//   } catch (error) {
+//     hide();
+//     message.error('删除失败，请重试');
+//     return false;
+//   }
+// };
 
 const TableList = () => {
-  const [createModalVisible, handleModalVisible] = useState(false);
-  const [updateModalVisible, handleUpdateModalVisible] = useState(false);
-  const [stepFormValues, setStepFormValues] = useState({});
-  const [actionRef, setActionRef] = useState();
+  // const [createModalVisible, handleModalVisible] = useState(false);
+  // const [updateModalVisible, handleUpdateModalVisible] = useState(false);
+  // const [stepFormValues, setStepFormValues] = useState({});
+  // const [actionRef, setActionRef] = useState();
   const [selectedRowKeys, setSelectedRowKeys] = useState([1042]);
   const [data, setData] = useState([]);
   const [pagination, setPagination] = useState({});
   const [loading, setLoading] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [value, setValue] = useState(1);
 
   const fetch = (params = {}) => {
     setLoading(true);
@@ -245,9 +285,38 @@ const TableList = () => {
     查看详情（市操作员、县操作员），查看地图（市操作员、县操作员），查看反馈报告（市操作员、县操作员、具体勘查人员），归档（市操作员、县操作员），
     reopen（市操作员、县操作员），填写反馈报告（具体勘查人员）"
     >
+      <Card bordered={false} style={{ marginBottom: '24px' }}>
+        <Row>
+          <Col span={6}>
+            <Input
+              placeholder="查询..."
+              prefix={<Icon type="search" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              suffix={<Icon type="ellipsis" style={{ color: 'rgba(0,0,0,.45)' }} />}
+            />
+          </Col>
+          <Col
+            span={10}
+            offset={8}
+            style={{
+              textAlign: 'right',
+            }}
+          >
+            <Button type="primary" icon="plus" onClick={() => setVisible(true)}>
+              审批
+            </Button>
+            <Divider type="vertical" />
+            <Dropdown overlay={menu}>
+              <Button icon="download">
+                导出 <Icon type="down" />
+              </Button>
+            </Dropdown>
+          </Col>
+        </Row>
+      </Card>
+
       <Card bordered={false}>
         <Tabs tabBarExtraContent={operations}>
-          <TabPane tab="已接收" key="1">
+          <TabPane tab="未开始" key="1">
             <Table
               loading={loading}
               rowKey="key"
@@ -259,15 +328,30 @@ const TableList = () => {
               onChange={handleTableChange}
             />
           </TabPane>
-          <TabPane tab="Tab 2" key="2">
-            aaaa
+          <TabPane tab="正在进行" key="2">
+            正在开发中...
           </TabPane>
-          <TabPane tab="Tab 3" key="3">
-            aaaa
+          <TabPane tab="已结束" key="3">
+            正在开发中...
           </TabPane>
         </Tabs>
       </Card>
-      <ProTable
+      <Modal
+        title="审批"
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+      >
+        <Group onChange={e => setValue(e.target.value)} value={value}>
+          <Radio style={radioStyle} value={1}>
+            根据行政区规划（默认）
+          </Radio>
+          <Radio style={radioStyle} value={2}>
+            自定义
+          </Radio>
+        </Group>
+      </Modal>
+      {/* <ProTable
         style={{ marginTop: '24px' }}
         headerTitle={<FormattedMessage id="menu.list.table-list" defaultMessage="Sales Ranking" />}
         onInit={setActionRef}
@@ -319,9 +403,9 @@ const TableList = () => {
         )}
         request={params => queryRule(params)}
         columns={columns}
-      />
+      /> */}
 
-      <CreateForm
+      {/* <CreateForm
         onSubmit={async value => {
           const success = await handleAdd(value);
 
@@ -351,7 +435,7 @@ const TableList = () => {
           updateModalVisible={updateModalVisible}
           values={stepFormValues}
         />
-      ) : null}
+      ) : null} */}
     </PageHeaderWrapper>
   );
 };
