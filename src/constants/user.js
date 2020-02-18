ebf276a933c7197ca5be8bb1e1d9c4ee138cc22c
White Ace <whiteace@sina.com>
2020-02-18 15:18:10 +0800

    fix

---
+++ src/constants/user.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Random } from 'mockjs';
+
+export default {
+  username: Random.cname(),
+  age: 7,
+  dogYears: 49,
+  id: 'c02c47fb44e6f4564952',
+  userid: 'c02c47fb44e6f4564952',
+  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/dgclegg/128.jpg',
+  email: '_@yahoo.com',
+  title: 'Senior Division Director',
+  signature: 'Reprehenderit mollitia odit non laboriosam sunt explicabo ipsam sed voluptas.',
+  group: 'Central',
+  tags: [
+    { key: '0', label: 'natus' },
+    { key: '1', label: 'quis' },
+    { key: '2', label: 'sapiente' },
+    { key: '3', label: 'odio' },
+    { key: '4', label: 'aliquid' },
+  ],
+  notifyCount: 8,
+  unreadCount: 8,
+  country: 'Cocos (Keeling) Islands',
+  geographic: {
+    province: { label: '浙江省', key: '275883' },
+    city: { label: '上 叶', key: '083584' },
+  },
+  address: '侯汉市, 绍辉 桥 8964 君浩 侬 藏',
+};
