8a6d4e7628c0bf0fe312e6cb0332c774e2a6b74c
White Ace <whiteace@sina.com>
2020-04-29 16:15:27 +0800

    fix

--- src/pages/user/login/components/Login/map.jsx
+++ src/pages/user/login/components/Login/map.jsx
@@ -1,11 +1,12 @@
 import { LockTwoTone, MailTwoTone, MobileTwoTone, UserOutlined } from '@ant-design/icons';
 import React from 'react';
 import styles from './index.less';
+
 export default {
   UserName: {
     props: {
       size: 'large',
-      id: 'userName',
+      id: 'username',
       prefix: (
         <UserOutlined
           style={{
