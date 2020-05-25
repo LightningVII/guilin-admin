6ce7d36541b6760d0d73c39cc159c6456100d458
White Ace <whiteace@sina.com>
2020-05-25 10:33:50 +0800

    fix: login service

--- src/services/login.js
+++ src/services/login.js
@@ -1,26 +1,8 @@
 import request from '@/utils/request';
-import { stringify } from 'querystring';
+import { stringify as str } from 'querystring';
 
-export async function fakeAccountLogin(params) {
-  return request(`/strapi/login`, {
-    method: 'POST',
-    data: stringify(params),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded;',
-    },
-  });
-}
+export const fakeAccountLogin = async params =>
+  request.post('/strapi/login', { data: str(params) });
 
-export async function getFakeCaptcha(mobile) {
-  return request(`/api/login/captcha?mobile=${mobile}`);
-}
-
-export async function updatePassword(params) {
-  return request('/strapi/sys/user/updatePassword', {
-    method: 'POST',
-    data: stringify(params),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded;',
-    },
-  });
-}
+export const updatePassword = async params =>
+  request.post('/strapi/sys/user/updatePassword', { data: str(params) });
